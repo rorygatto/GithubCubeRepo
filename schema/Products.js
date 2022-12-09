1fe4729fb7017e1d5f0129a4c28e838317316af2
rorygatto <rorygatto@gmail.com>
2022-12-09 17:52:52 +0000

    "Initial commit"

---
+++ schema/Products.js
@@ -0,0 +1,60 @@
+cube(`Products`, {
+  sql: `SELECT * FROM public.products`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+  
+  joins: {
+    Suppliers: {
+      sql: `${CUBE}.supplier_id = ${Suppliers}.id`,
+      relationship: `belongsTo`
+    },
+    
+    ProductCategories: {
+      sql: `${CUBE}.product_category_id = ${ProductCategories}.id`,
+      relationship: `belongsTo`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, name, createdAt]
+    }
+  },
+  
+  dimensions: {
+    supplierId: {
+      sql: `supplier_id`,
+      type: `number`
+    },
+    
+    productCategoryId: {
+      sql: `product_category_id`,
+      type: `number`
+    },
+    
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true
+    },
+    
+    name: {
+      sql: `name`,
+      type: `string`
+    },
+    
+    description: {
+      sql: `description`,
+      type: `string`
+    },
+    
+    createdAt: {
+      sql: `created_at`,
+      type: `time`
+    }
+  }
+});
