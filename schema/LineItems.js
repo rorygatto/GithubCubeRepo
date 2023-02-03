f1244605ce8a53efc47ff32d827bb0e90653ee1b
rorygatto <rorygatto@gmail.com>
2023-02-03 19:34:30 +0000

    a

--- schema/LineItems.js
+++ schema/LineItems.js
@@ -1,57 +1,52 @@
 cube(`LineItems`, {
   sql: `SELECT * FROM public.line_items`,
-  
-  preAggregations: {
-    // Pre-Aggregations definitions go here
+  preAggregations: {// Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+
+    longBuild: {
+      measures: [LineItems.count],
+      timeDimension: LineItems.createdAt,
+      granularity: `second`
+    }
   },
-  
   joins: {
     Products: {
       sql: `${CUBE}.product_id = ${Products}.id`,
       relationship: `belongsTo`
     },
-    
     Orders: {
       sql: `${CUBE}.order_id = ${Orders}.id`,
       relationship: `belongsTo`
     }
   },
-  
   measures: {
     count: {
       type: `count`,
       drillMembers: [id, createdAt]
     },
-    
     quantity: {
       sql: `quantity`,
       type: `sum`
     },
-    
     price: {
       sql: `price`,
       type: `sum`
     }
   },
-  
   dimensions: {
     productId: {
       sql: `product_id`,
       type: `number`
     },
-    
     orderId: {
       sql: `order_id`,
       type: `number`
     },
-    
     id: {
       sql: `id`,
       type: `number`,
       primaryKey: true
     },
-    
     createdAt: {
       sql: `created_at`,
       type: `time`
