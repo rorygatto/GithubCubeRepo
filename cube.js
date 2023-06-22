88b8df15c873778ae7a8b4afe2fa066ff573d4bc
rorygatto <rorygatto@gmail.com>
2023-06-22 14:37:35 +0000

    order

--- cube.js
+++ cube.js
@@ -1,9 +1,25 @@
 // Cube.js configuration options: https://cube.dev/docs/config
-
-// NOTE: third-party dependencies and the use of require(...) are disabled for
-// CubeCloud users by default.  Please contact support if you need them
-// enabled for your account.  You are still allowed to require
-// @cubejs-backend/*-driver packages.
-
+function addDimstoOrderBy(query) {
+  // Code to handle sort order for pagination
+  // We need to make sure dimensions are included in sorting AFTER order!
+  let orderDims = new Array();
+  for (let i = 0; i < query.order.length; i++) {
+    let originalOrder = query.order[i];
+    orderDims.push(originalOrder[0]);
+  };
+  for (let i = 0; i < query.dimensions.length; i++) {
+    let queryDim = query.dimensions[i];
+    if (!orderDims.includes(queryDim)) {
+      query.order.push([queryDim,'asc']);
+      }
+  };
+  return query;
+}
 module.exports = {
+  queryRewrite: (query, { securityContext }) => {
+    // Fix the ordering
+    query = addDimstoOrderBy(query);
+    // Return the query
+    return query;
+  },
 };
